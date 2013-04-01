fd41e297daf2b45a64c63ac4dc3399b246ba98b0
Michiel van Oosten <michieljoris@gmail.com>
2013-04-02 06:03:05 +1000

    refactored, more functionality

--- html-builder.js
+++ html-builder.js
@@ -6,20 +6,26 @@
 // var sys = require('sys');
 
 var Plates = require('plates');
+var util = require('util');
 var fs = require('fs');
 // var path = require('path');
 var htmlFormatter = require('./html-formatter.js');
 var md = require("node-markdown").Markdown;
-var filemon = require('filemonitor');
+var filemon = require('./filemonitor');
 // var sys = require('sys');
 // var exec = require('child_process').exec;
 var argv = require('optimist').argv;
 
 
-var cachedPartials = {};
-var buildData;
 var log;
 
+var defaultPartials = {
+   _linkBlock: '', _scriptBlock: '' 
+    ,script: '<script type="text/javascript" src="bla"></script>'
+    ,link:'<link rel="stylesheet" type="text/css" href="">'
+};
+var partialsCollection = {};
+var monitoredDirs;
 
 
 function saveFile(name, str){
@@ -39,32 +45,43 @@ function trailWith(str, trail) {
     return str ? (str + (!endsWith(str, trail) ? trail : '')) : undefined;
 }
 
-
-function getPartial(name) {
+function getPartial(partialsPath, name) {
     var partial, path; 
+    // log('getting partial', name);
+    if (name.indexOf('.') === -1) {
+        partial = partialsCollection[name];   
+        if (partial) return partial;
+        name += '.html';
+    }
+    // log('searching for partial on disk');
     var isMarkdown = endsWith(name, '.md') || endsWith(name, '.markdown');
-    // if (cachedPartials[name]) return cachedPartials[name];
     if (!isMarkdown) name = trailWith(name, '.html');
     try {
-        path = buildData.basePath + buildData.partialsPath + name;
+        path = partialsPath + name;
         partial = fs.readFileSync(path, 'utf8');
+        if (isMarkdown) partial = md(partial);
     } catch(e) {
-        console.log("Couldn't find partial " + buildData.partialsPath + name);
+        // console.log("Couldn't find partial " + partialsPath + name);
+    }
+    if (!partial) {
+        log("Couldn't find partial " + partialsPath + name);
         partial = makeTag('div', {
             'class': 'row',
             style: 'margin-left: 0; padding-left:10px; border:solid grey 1px; height:40; width:100%;' 
             ,innerHtml: 'placeholder for ' + name
         });
     }
-    if (isMarkdown) partial = md(partial);
-    cachedPartials[name] = partial;
     return partial;   
 }
 
-function makeStyleBlock(path, array) {
+function makeStyleBlock(args) {
+    // console.log('Making style block\n', args);
+    var path = trailWith(args.path, '/') || 'css/';
+    var array = args.files;
+    
     var map = Plates.Map();
     map.where('rel').is('stylesheet').use('data').as('href');
-    var style = getPartial('style'); 
+    var style = getPartial(args.partialsDir, 'link'); 
     var result = '';
     array.forEach(function(e) {
         if (e instanceof Object) {
@@ -89,10 +106,14 @@ function makeStyleBlock(path, array) {
     return result + '\n';   
 }
 
-function makeScriptBlock(path, array) {
+function makeScriptBlock(args) {
+    // console.log('Making script block\n', args);
+    var path = trailWith(args.path, '/') || 'js/';
+
+    var array = args.files;
     var map = Plates.Map();
     map.where('type').is('text/javascript').use('data').as('src');
-    var script = getPartial('script'); 
+    var script = getPartial(args.partialsDir, 'script'); 
     var result = '';
         array.forEach(function(e) {
             e = trailWith(e, '.js');
@@ -117,31 +138,14 @@ function makeTag(tag, attrs, unary) {
     return result;   
 }
 
-function makeUnaryTags(tag, attrCollection) {
-    var result = '';
-    attrCollection = attrCollection || [];
-    attrCollection.forEach(function(attrs) {
-        result += makeTag(tag, attrs, true);
-    });
-    return  result + '\n';   
-}
 
-
-function wrap(string, tag) {
-    var partial = getPartial(tag);
+function buildMenuTree(tree) {
     
-    var data = {};
-    data[tag] = string;
-    return Plates.bind(partial, data); 
-}
-
-
-function buildMenu(menu) {
+   tree = tree || [];
     
-    menu = menu || [];
-    
-    var str = '<div class="ie-dropdown-fix" > <div id="navigation">' +
-        '<ul id="nav" class="menu sf-menu">';
+    // var str = '<div class="ie-dropdown-fix" > <div id="navigation">' +
+    //     '<ul id="nav" class="menu sf-menu">';
+    var str = '';
     
     function makeLi(entry) {
         
@@ -163,42 +167,74 @@ function buildMenu(menu) {
         return li;
     }
     
-    menu.forEach(function(e){
+    tree.forEach(function(e){
         str += makeLi(e); 
     });
     
 
-    var end = '</ul></div></div><div class="clear"></div>';
-    str += end;   
+    // var end = '</ul></div></div><div class="clear"></div>';
+    // str += end;   
     return str;
 }
 
+function addTo_Blocks(js, css) {
+    partialsCollection._scriptBlock += makePartial('scriptBlock', { files: js});
+    partialsCollection._linkBlock += makePartial('linkBlock', { files: css});
+} 
 
-function makeSequenceSlider(data) {
+function makeMenu(args) {
+    var menus = {
+        
+        superfish: { 
+            start: '<div class="ie-dropdown-fix" > <div id="navigation">' +
+                '<ul id="nav" class="menu sf-menu">',
+            end: '</ul></div></div><div class="clear"></div>',
+            js : [
+                'hoverIntent'
+                ,'superfish'
+                ,'startSuperfish'
+            ],
+            css : ['superfish'] }
+        ,css: {
+            start: '<div class="ie-dropdown-fix" > <div id="navigation">' +
+                '<ul id="nav" class="menu">',
+            end: '</ul></div></div><div class="clear"></div>',
+            js: [],
+            css: ['menu']
+        }
+    };
+    var menu = menus[args.type];
+    if (!menu) return '';
+    addTo_Blocks(menu.js, menu.css);
+    return menu.start + buildMenuTree(args.tree) + menu.end;
+    
+}
+
+function makeSequenceSlider(slides) {
     var js = [
             'sequence.jquery-min'
             ,'startSequence'
     ];
     var css = ['slidein-seqtheme'];
-    data.scripts = data.scripts.concat(js);
-    data.styles = data.styles.concat(css);
+    addTo_Blocks(js, css);
+    return '';
     //TODO
 }
 
-function makeFlexSlider(data) {
+function makeFlexSlider(slides) {
     var js = [
-        'jquery.flexslider-min',
-        'startFlex'
+        'jquery.flexslider-min'
+        // 'startFlex'
     ];
     var css = ['flexslider'];
-    data.scripts = data.scripts.concat(js);
-    data.styles = data.styles.concat(css);
+    addTo_Blocks(js, css);
+    
     function makeSlide(s) {
         return '<li><img src=' + s.url + 
             '><div class="slide-caption"><h3>' + 
             s.title + '</h3> </div> </li>';
     }
-        var slides = data.slides; 
+    // var slides = args.slides; 
     var str ='<div class="flexslider"><ul class="slides">';
     slides.forEach(function(s) {
         str += makeSlide(s);   
@@ -207,7 +243,7 @@ function makeFlexSlider(data) {
     return str;
 }
 
-function makeCameraSlider(data) {
+function makeCameraSlider(slides) {
     var js = [
         'jquery.mobile.customized.min'
         ,'jquery.easing.1.3'
@@ -215,15 +251,14 @@ function makeCameraSlider(data) {
         ,'startCamera'
     ];
     var css = ['camera'];
-    data.scripts = data.scripts.concat(js);
-    data.styles = data.styles.concat(css);
+    addTo_Blocks(js, css);
     
     function makeSlide(s) {
         return '<div data-src=' + s.url +
             '><div class="camera_caption fadeFromLeft"><h4>' +
             s.title + '</h4>' + s.subtitle + '</div></div>'; 
     }
-    var slides = data.slides; 
+    // var slides = args.slides; 
     var str= '<div id="camera" class="camera_wrap">';
     slides.forEach(function(s) {
         str += makeSlide(s);   
@@ -232,83 +267,97 @@ function makeCameraSlider(data) {
     return str;
 }
 
-var makeSlideShow = {
-    camera: makeCameraSlider,
-    flex: makeFlexSlider,
-    sequence: makeSequenceSlider
-};
 
-function render() {
-    var paths = buildData.paths || {};
-    paths.js = trailWith(paths.js, '/') || 'js/';
-    paths.css = trailWith(paths.css, '/') || 'css/';
-    var slideShowHtml = '';
-    if (buildData.slideShow)
-        slideShowHtml = makeSlideShow[buildData.slideShow](buildData);
+function makeSlideShow(args) {
+    var makers = {
+        camera: makeCameraSlider,
+        flex: makeFlexSlider,
+        sequence: makeSequenceSlider
+    };
+    if (!makers[args.type]) return '';
+    return makers[args.type](args.slides);
+}
 
-    buildData.styles = buildData.styles || [];
-    buildData.scripts = buildData.scripts || [];
+function render(args) {
+    var partialsDir = args.partialsDir;
     
-    // var styleSwitcherHtml = '';
-    // var styleSwitcherLink = '';
-    // if (buildData.styleSwitcher) {
-    //     styleSwitcherHtml = getPartial('styles-switcher');
-    //     buildData.scripts.push('styles-switcher');
-    //     buildData.styles.push('styles-switcher');
-    //     // options.styles.push({name: 'ribbons', id: 'ribbons'});
-    // }
-    
-    var metaTags = makeUnaryTags('meta', buildData.metaTags);
-    var styleBlock = makeStyleBlock(paths.css, buildData.styles);
-    var scriptBlock = makeScriptBlock(paths.js, buildData.scripts);
-    var title = '<title>' + buildData.title + '</title>';
-    var head = wrap(title + metaTags + styleBlock + scriptBlock, 'head');
-
-    var layoutIdPrefix = buildData.layoutIdPrefix || 'layout';
-    var layout = buildData.layoutPartial || 'layout';
-    layout = getPartial(layout);
-    
-    var partials = buildData.partials || {};
-    
-    Object.keys(partials).forEach(function(p) {
-        var html = getPartial(partials[p]);
+    var template = getPartial(partialsDir, args.src);
+        
+    args.mapping = args.mapping || [];
+    Object.keys(args.mapping).forEach(function(tagId) {
+        var partialIds = args.mapping[tagId];
+        partialIds = util.isArray(partialIds) ? partialIds : [partialIds];
+        
+        var html = '';
+        partialIds.forEach(function(partialId) {
+            html += getPartial(partialsDir, partialId);
+        });
         var selector = {};
-        selector[layoutIdPrefix + '-' +  p] = html;
-        layout = Plates.bind(layout, selector); 
+        selector[tagId + args.tagIdPostfix] = html;
+        template = Plates.bind(template, selector); 
     });
-    var menuHtml = buildMenu(buildData.menu);
-    var menu = { "layout-menu": menuHtml };
-    layout = Plates.bind(layout, menu); 
-    
-    var slideShow = { "layout-slideShow": slideShowHtml };
-    layout = Plates.bind(layout, slideShow); 
-    
-    // var logoHtml = getPartial('logo');
-    // var logo = { "partial-logo": logoHtml };
-    // layout = Plates.bind(layout, logo); 
-    // layout += styleSwitcherHtml;
-    
-    var body = wrap(layout, 'body');
-    var output = '<!doctype html>\n' +
-        '<!--[if IE 7 ]><html ng-app class="ie ie7" lang="en"><![endif]-->\n' +
-        '<!--[if IE 8 ]><html ng-app class="ie ie8" lang="en"><![endif]-->\n' +
-        '<!--[if (gte IE 9)|!(IE)]><!--><html ng-app lang="en"><!--<![endif]-->\n\n' + 
-        head + body +
-        '\n</html>';
-    
-    if (buildData.prettyPrintHtml) {
-        output = htmlFormatter.format(output,{
+   
+    if (args.prettyPrintHtml) {
+        template = htmlFormatter.format(template,{
             indentSize: 4,
             maxLineLength: 10,
             indent: 2
         });
     }
-
-    saveFile(buildData.basePath + buildData.out, output);
-    log('Created ' + buildData.out);
-
-    
+    if (args.out) {
+        saveFile(args.baseDir + args.out, template);   
+        log('Created ' + args.out);
+    }
+    // log(template);
+    log('Processed template ' + args.src);
+    log('------------------------------');
+    return template;
 }
+
+function addProperties(o1,o2) {
+    var newObject = {};
+    o1 = o1 || {};
+    o2 = o2 || {};
+    Object.keys(o1).forEach(function(k) {
+        newObject[k] = o1[k];
+    });
+    Object.keys(o2).forEach(function(k) {
+        newObject[k] = o2[k];
+    });
+    return newObject;
+} 
+
+function makeUnaryTags(args) {
+    var tag = args.tagType;
+    var attrCollection = args.tags;
+    var result = '';
+    attrCollection = attrCollection || [];
+    attrCollection.forEach(function(attrs) {
+        result += makeTag(tag, attrs, true);
+    });
+    return  result + '\n';   
+}
+
+function addDirToMonitor(partial) {
+   if (partial.partialsDir && monitoredDirs.indexOf(partial.partialsDir) === -1)
+       monitoredDirs.push(partial.partialsDir);
+}
+
+function processPartials(partials) {
+    partialsCollection = addProperties(defaultPartials, partials.ids);
+    Object.keys(partials).forEach(function(k) {
+        addDirToMonitor(partials[k]);
+        partials[k] = partials[k] || [];
+        partials[k] = util.isArray(partials[k]) ? partials[k] : [partials[k]];
+        partials[k].forEach(function(d) {
+            var partial = makePartial(k, d);
+            if (d.id) partialsCollection[d.id] = partial;
+        });
+    });
+    // log(util.inspect(partialsCollection, { colors: true }));
+}
+
+
 
 function evalFile(fileName) {
     var file;
@@ -322,7 +371,7 @@ function evalFile(fileName) {
         }
 } 
 
-function monitor(buildjsfile, target) {
+function monitor(dataFileName, target) {
     var isHtml = /.*\.html?$/;
     var isMdown = /.*\.mdown?$/;
     var isMarkdown = /.*\.markdown?$/;
@@ -341,64 +390,121 @@ function monitor(buildjsfile, target) {
         var i = ev.filename.lastIndexOf('/');
         var dir = ev.filename.slice(0, i+1);
         // log(dir, ev.filename);
-        if (ev.filename === buildjsfile ||
-            (dir === target && (
+        if (ev.filename === dataFileName ||
+            (target.indexOf(dir) !== -1 && (
                 isMdown.test(ev.filename) ||
                 isMarkdown.test(ev.filename) ||
                isMd.test(ev.filename) || 
-            isHtml.test(ev.filename)))) {
+                    isHtml.test(ev.filename)))) {
             // log(ev.timestamp);
             if (lastEvent.timestamp.toString() === ev.timestamp.toString() &&
                 lastEvent.filename === ev.filename) return;
             lastEvent = ev;
             log('Modified>> ' + ev.filename);
+            filemon.stop(function() {
+                build();
+                
+            });
             // log('Building ' + buildData.out);
             // exec("lispy -r " + ev.filename, puts);
-            buildData = evalFile(file);
-            buildData.partialsPath = trailWith( buildData.partialsPath, '/');
-            // log(buildData.title);
+            // var buildData = evalFile(dataFileName);
+            // buildData.partialsPath = trailWith( buildData.partialsPath, '/');
+            // // log(buildData.title);
             
-            render();
+            // render();
             // log("Event " + ev.eventId + " was captured for " +
             //             filetype + " " + ev.filename + " on time: " + ev.timestamp.toString());
             // }
         }
     };
-    var i = buildjsfile.lastIndexOf('/');
-    var dir = buildjsfile.slice(0, i+1);
+    var i = dataFileName.lastIndexOf('/');
+    var dir = dataFileName.slice(0, i+1);
+    target.push(dir);
     log(dir);
     var options = {
-        target: [dir, target],
+        target: target,
         // recursive: true,
         listeners: {
             modify: onFileEvent
         }
     };
     
-    log('Watching ' + target + ' and ' + buildjsfile);
-    filemon.watch(options);
+    log('Watching ' + target + ' and ' + dataFileName);
+    filemon.watch(options); 
 } 
+
+
+var builders = {
+    metaBlock: { f: makeUnaryTags, defArgs: { tagType: 'meta'}}
+    ,linkBlock: { f: makeStyleBlock }
+    ,scriptBlock: { f: makeScriptBlock }
+    ,slideShow:  { f: makeSlideShow }
+    ,menu: { f: makeMenu }
+    ,template: { f: render }
+};
+
+function makePartial(name, args) {
+    var maker = builders[name];
+    if (!maker) return '';
+    args = addProperties(maker.defArgs, args);
+    return maker.f(args);
+}
+
+var testing = true;
+function build() {
+    // var dataFileName = (argv._ && argv._[0]) || argv.file || '/home/michieljoris/www/firstdoor/data.js';
+    var dataFileName = (argv._ && argv._[0]) || argv.file || process.cwd() + '/data.js';
+    
+    // try {
+    var buildData = evalFile(dataFileName);
+    if (!buildData) throw Error('buildData is undefined!!');
+    
+    buildData.baseDir = trailWith(buildData.baseDir || process.cwd(), '/');
+    buildData.partialsDir = trailWith( buildData.partialsDir || 'partials', '/');
+    buildData.tagIdPostfix = buildData.tagIdPostfix || '__';
+        
+    log = !buildData.verbose || !testing ?  function () {}: function() {
+        console.log.apply(console, arguments); };
+    log('Cwd: ' + process.cwd());
+    log('Base dir: ' + buildData.baseDir);
+    
+    var partialsDir = buildData.baseDir + buildData.partialsDir;
+    builders.template.defArgs = {
+        baseDir: buildData.baseDir,
+        partialsDir: partialsDir,
+        tagIdPostfix: buildData.tagIdPostfix,
+        prettyPrintHtml: buildData.prettyPrintHtml
+    };
+    
+    builders.linkBlock.defArgs = {
+        partialsDir: partialsDir,
+        css: 'css/'
+    };
+    
+    builders.scriptBlock.defArgs = {
+        partialsDir: partialsDir,
+        js: 'js'
+    };
+    
+    monitoredDirs = [];
+    monitoredDirs.push(partialsDir);
+    processPartials(buildData.partials || {});
+    
+    // render();
+    log('Finished rendering');
+        
+    if (buildData.monitor) monitor(dataFileName, monitoredDirs);
+    // } catch (e) {
+    //     console.log(dataFileName + ' is invalid, or doesn\'t exist!!\n', e);
+    // }
+
+}
 
 if (argv.h || argv.help) {
     console.log([
-        "usage: html-builder [pathToData.jsFile]"
+        "usage: html-builder [pathToData.js]"
     ].join('\n'));
     process.exit();
 }
 
-var file = (argv._ && argv._[0]) || argv.file || process.cwd() + '/data.js';
-    
-try {
-    buildData = evalFile(file);
-    buildData.partialsPath = trailWith( buildData.partialsPath, '/');
-    
-    log = !buildData.verbose ?  function () {}: function() {
-        console.log.apply(console, arguments); };
-    log('Cwd: ' + process.cwd());
-    render();
-    if (buildData.monitor) monitor(file, buildData.basePath + buildData.partialsPath);
-} catch (e) {
-    console.log(file + ' doesn\'t exist!!');
-}
-
-
+build();
